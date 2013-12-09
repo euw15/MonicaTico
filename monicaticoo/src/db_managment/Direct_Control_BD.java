c9d2dddd307502578de58d61c1f850ae077d1a1c
omontes <pulgamontes@gmail.com>
2013-12-09 15:24:46 -0600

    Se agrego el metodo para obtener el valor del inventario y se inserto en sql_files ValorInventario.sql

--- monicaticoo/src/db_managment/Direct_Control_BD.java
+++ monicaticoo/src/db_managment/Direct_Control_BD.java
@@ -5,7 +5,12 @@
 package db_managment;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 
@@ -24,8 +29,45 @@ public class Direct_Control_BD {
         this.statement=statement;
         
     }
-    
+    /**
+     * Obtiene el valor del inventario segun el nombre del inventario
+     * @param nombreInv 
+     */
+    public void valorInventario(String nombreInv){
+        try{
+            String valorInventario=this.readSql("../monicaticoo/src/sql_files/"
+                    + "ValorInventario.sql");
+            PreparedStatement stm = this.conection.prepareStatement
+                    (valorInventario);
+            stm.setString(1,nombreInv);
+            ResultSet resultset =stm.executeQuery();
+            //Imprime el resultado obtenido del valor del inventario
+            while (resultset.next()) {
+                System.out.println(resultset.getString(1)+
+                        "||" +resultset.getString(2)+ "||" +resultset.getInt(3)
+                        + "||" +resultset.getInt(4));                
+            }
+            
+        }
+        catch (Exception e) {
+            System.out.println("Error al obtener el valor del inventario");
+        }
+            
     
 
     
+}
+     private String readSql(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String ls = System.getProperty("line.separator");
+
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+
+        return stringBuilder.toString();
+    }
 }
---
+++ monicaticoo/src/sql_files/ValorInventario.sql
@@ -0,0 +1,4 @@
+SELECT P.idProducto,P.Nombre,P.Precio,P.Precio*I.Cantidad AS ValorVenta
+FROM inventario AS Inv,inventarioproducto AS I,producto AS P
+WHERE I.idInventarioProducto=Inv.idInventario AND P.idProducto=I.idProducto
+AND Inv.Descripcion=?;
